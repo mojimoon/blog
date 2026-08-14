ce4cce6e109953f399341696474592fd69df9adc
mojimoon <cryovit@outlook.com>
2026-08-14 21:11:05 +0800

    Site updated: 2026-08-14 21:11:02

--- js/services/mdrender.js
+++ js/services/mdrender.js
@@ -1,12 +1,1 @@
-utils.jq(() => {
-  const els = document.getElementsByClassName('ds-mdrender');
-  for (var i = 0; i < els.length; i++) {
-    const el = els[i];
-    const src = `${el.getAttribute('src')}?t=${new Date().getTime()}`;
-    
-    utils.request(el, src, async resp => {
-      const data = await resp.text();
-      el.innerHTML = marked.parse(data);
-    });
-  }
-});
+utils.jq(()=>{for(var e=document.getElementsByClassName("ds-mdrender"),r=0;r<e.length;r++){let t=e[r];var a=t.getAttribute("src")+"?t="+(new Date).getTime();utils.request(t,a,async e=>{e=await e.text();t.innerHTML=marked.parse(e)})}});
